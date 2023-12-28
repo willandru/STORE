a74e3a0c417792231319dd3e1b16425cb5021ae3
willandru <willandru@hotmail.com>
2023-12-28 16:17:07 -0500

    Evrething working nice

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,25 @@
-const Navbar = () => {
+import Link from "next/link";
+
+import Container from "@/components/ui/container";
+import MainNav from "./main-nav";
+import getCategories from "@/actions/get-categories";
+
+
+export const revalidate =0; // SO IT NEVER CASH BuT NOT WORKING
+
+const Navbar = async () => {
+    const categories = await getCategories();
+
     return(
         <div className="border-b">
-            Navbar¡
+            <Container>
+               <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
+               <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
+                    <p className="font-bold text-xl"> STORE</p>                
+                </Link>
+                <MainNav data={categories} />
+               </div>
+            </Container>
         </div>
     );
 }
