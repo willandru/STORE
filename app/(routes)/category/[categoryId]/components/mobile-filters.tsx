02689ee5a66e07bd8076301dfbe730c01578a0ac
willandru <willandru@hotmail.com>
2024-01-05 16:05:34 -0500

    Expanding works

--- app/(routes)/category/[categoryId]/components/mobile-filters.tsx
+++ app/(routes)/category/[categoryId]/components/mobile-filters.tsx
@@ -1,9 +1,12 @@
 "use client";
 
 import Button from "@/components/ui/button";
+import IconButton from "@/components/ui/icon-button";
 import { Color, Size } from "@/types";
-import { Plus } from "lucide-react";
+import { Dialog } from "@headlessui/react";
+import { Plus, X } from "lucide-react";
 import { useState } from "react";
+import Filter from "./filter";
 
 interface MobileFiltersProps{
     sizes: Size[];
@@ -22,10 +25,43 @@ const MobileFilters: React.FC<MobileFiltersProps> =({
 
     return(
         <>
-        <Button className="flex items-center gap-x-2 lg:hidden">
+        <Button onClick={onOpen} className="flex items-center gap-x-2 lg:hidden">
             Filters
             <Plus size={20}/>
         </Button>
+
+        <Dialog open={open} as="div" className="relative z-40 lg:hidden" onClose={onClose}>
+            {/* Background  */}
+            <div className="fixed inset-0 bg-black bg-opacity-25" />
+
+            {/* Dialog POsition  */}
+            <div className="fixed inset-0 z-40 flex">
+                <Dialog.Panel className="relative ml-auto flex h-full w-full max-w-xs flex-col overflow-y-auto bg-white py-4 pb-6 shadow-xl">
+                    {/* Clsoe BUtton  */}
+                    <div className="flex imtes-center justify-end px-4">
+                        <IconButton  icon={<X />} onClick={onClose}/>
+
+                    </div>
+
+                    {/* Render the filters  */}
+                    <div className="p-4">
+                            <Filter 
+                                valueKey="sizeId"
+                                name="Sizes"
+                                data={sizes}
+                            />
+                            <Filter 
+                                valueKey="colorId"
+                                name="Colors"
+                                data={colors}
+                            />
+
+                    </div>
+                </Dialog.Panel>
+            </div>
+
+
+        </Dialog>
         </>
     );
 }
